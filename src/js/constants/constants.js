f897ae424fde4364117dc85866e59775bb26a301
Millie <jay.lee.2280@gmail.com>
2022-03-30 20:00:44 +0900

    Add: category.json data & modify server.js, constants.js

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -2,4 +2,5 @@ export const URL = {
   keyword: 'http://localhost:3000/autocomplete?keyword=',
   banner: 'http://localhost:3000/banner',
   searchCategory: 'http://localhost:3000/searchCategory',
+  category: 'http://localhost:3000/category',
 };
